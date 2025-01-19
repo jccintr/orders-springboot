d7dcf05f1513c18db0941ac51c72e901168f5306
Julio Cesar <jccintr@gmail.com>
2025-01-19 16:43:10 -0300

    Criado Enum OrderStatus

---
+++ src/main/java/com/jcsoftware/orders/entities/enums/OrderStatus.java
@@ -0,0 +1,29 @@
+package com.jcsoftware.orders.entities.enums;
+
+public enum OrderStatus {
+     WAITING_PAYMENT(1),
+     PAID(2),
+     SHIPPED(3),
+     DELIVERED(4),
+     CANCELED(5);
+     
+     private int code;
+	
+	private OrderStatus(int code) {
+		this.code = code;
+	}
+	
+	public int getCode() {
+		return code;
+	}
+	
+	public static OrderStatus valueOf(int code) {
+		
+		for(OrderStatus os : OrderStatus.values()) {
+			if(os.getCode()==code) {
+				return os;
+			}
+		}
+		throw new IllegalArgumentException("Invalid Order Status code");
+	}
+}
