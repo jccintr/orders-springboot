2b2f03de093b528a1a5b7e6765db6b384b1b0fc3
Julio Cesar <jccintr@gmail.com>
2025-01-20 15:48:47 -0300

    Criado Payment Entity

--- src/main/java/com/jcsoftware/orders/config/TestConfig.java
+++ src/main/java/com/jcsoftware/orders/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.jcsoftware.orders.entities.Category;
 import com.jcsoftware.orders.entities.Order;
 import com.jcsoftware.orders.entities.OrderItem;
+import com.jcsoftware.orders.entities.Payment;
 import com.jcsoftware.orders.entities.Product;
 import com.jcsoftware.orders.entities.User;
 import com.jcsoftware.orders.entities.enums.OrderStatus;
@@ -81,5 +82,10 @@ public class TestConfig implements CommandLineRunner {
 		OrderItem oi3 = new OrderItem(o2, p3, 2, p3.getPrice());
 		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice()); 
         orderItemRepository.saveAll(Arrays.asList(oi1,oi2,oi3,oi4));
+        
+        Payment pay1 = new Payment(null,Instant.parse("2025-06-20T21:53:07Z"),o1);
+        o1.setPayment(pay1);
+        orderRepository.save(o1);
+        
 	}
 }
--- src/main/java/com/jcsoftware/orders/entities/Order.java
+++ src/main/java/com/jcsoftware/orders/entities/Order.java
@@ -9,6 +9,7 @@ import java.util.Set;
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.jcsoftware.orders.entities.enums.OrderStatus;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -16,6 +17,7 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToMany;
+import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 
 @Entity
@@ -40,7 +42,11 @@ public class Order implements Serializable {
 	@JoinColumn(name="client_id")
 	private User client;
 	
-	 @OneToMany(mappedBy = "id.order")
+	@OneToOne(mappedBy="order",cascade=CascadeType.ALL)
+	private Payment payment;
+	
+	
+	@OneToMany(mappedBy = "id.order")
 	private Set<OrderItem> items = new HashSet<>();
 	
 
@@ -95,6 +101,15 @@ public class Order implements Serializable {
 		}
 		
 	}
+	
+	 public Payment getPayment() {
+			return payment;
+		}
+
+		public void setPayment(Payment payment) {
+			this.payment = payment;
+		}
+
 
 	@Override
 	public int hashCode() {
