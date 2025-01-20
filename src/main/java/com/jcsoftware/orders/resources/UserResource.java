b4b588deebe115e4dced792c2b392045c1b50953
Julio Cesar <jccintr@gmail.com>
2025-01-20 19:46:47 -0300

    Criado inserção de users

--- src/main/java/com/jcsoftware/orders/resources/UserResource.java
+++ src/main/java/com/jcsoftware/orders/resources/UserResource.java
@@ -3,9 +3,12 @@ package com.jcsoftware.orders.resources;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -31,5 +34,10 @@ public class UserResource {
 		User user = service.findById(id);
 		return ResponseEntity.ok().body(user);
 	}
-	
+	@PostMapping
+	public ResponseEntity<User> insert(@RequestBody User newUser){
+		newUser = service.insert(newUser);
+		// return ResponseEntity.ok().body(newUser);
+		 return ResponseEntity.status(HttpStatus.CREATED).body(newUser);
+	}
 }
--- src/main/java/com/jcsoftware/orders/services/UserService.java
+++ src/main/java/com/jcsoftware/orders/services/UserService.java
@@ -25,4 +25,8 @@ public class UserService {
     	return user.get();
     	
     }
+    
+    public User insert(User newUser) {
+    	return repository.save(newUser);
+    }
 }
